c2029dead82538504b0497d56db875b1d9228814
suryasardar <suryaprakashchary515@gmail.com>
2024-06-11 17:45:36 +0530

    login design done

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -2,15 +2,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
  import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
-
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Dashboard/>} />
+          <Route index element={<Dashboard />} />
         </Route>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+
       </Routes>
     </BrowserRouter>
   );
--- Frontend/my-app/src/components/Dashboard.jsx
+++ Frontend/my-app/src/components/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
   return (
+    <>
     <div className='text-2xl'>Dashboard</div>
+     <Link to='/login' className='m-2'>
+     Login
+      </Link>
+      <Link to='/signup'>
+       signup
+     </Link>
+    </>
+
   )
 }
 
